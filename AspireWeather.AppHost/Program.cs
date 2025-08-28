60a7cb0204fec500561760b2f148ff26e10675df
Anton Lobarev <loborew@gmail.com>
2025-08-29 00:28:18 +0300

    even smaller fix

--- AspireWeather.AppHost/Program.cs
+++ AspireWeather.AppHost/Program.cs
@@ -1,18 +1,20 @@
-using Aspire.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
 // --- Внешние зависимости (контейнеры) ---
-var cache = builder.AddRedis("cache");
+var cache = builder.AddRedis("cache")
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithRedisInsight();
+
 var postgres = builder.AddPostgres("postgres");
 
 var username = builder.AddParameter("username", secret: true);
 var password = builder.AddParameter("password", secret: true);
 
 // Создаем RabbitMQ с явным пользователем и паролем
-var messaging = builder.AddRabbitMQ("messaging", username, password);
-
+var messaging = builder.AddRabbitMQ("messaging", username, password)
+    .WithManagementPlugin();
 
 // --- Базы данных ---
 // Создаем базу данных "userdb" в нашем контейнере Postgres
@@ -23,14 +25,16 @@ var userDb = postgres.AddDatabase("userdb");
 // UserApi зависит только от своей базы данных
 var userApi = builder.AddProject<Projects.AspireWeather_UserApi>("userapi")
                      .WithReference(userDb)
-                     .WaitFor(postgres);
+                     .WaitFor(userDb);
 
 // WeatherApi зависит от кэша, брокера сообщений и от UserApi
 var weatherApi = builder.AddProject<Projects.AspireWeather_WeatherApi>("weatherapi")
                         .WithReference(cache)
                         .WithReference(messaging)
                         .WithReference(userApi)
-                        .WaitFor(messaging);
+                        .WaitFor(cache)
+                        .WaitFor(messaging)
+                        .WaitFor(userApi);
 
 // AuditService зависит только от брокера сообщений
 var auditService = builder.AddProject<Projects.AspireWeather_AuditService>("auditservice")
@@ -40,6 +44,8 @@ var auditService = builder.AddProject<Projects.AspireWeather_AuditService>("audi
 // WebApp (UI) зависит от обоих API для получения данных
 builder.AddProject<Projects.AspireWeather_WebApp>("webapp")
        .WithReference(weatherApi)
-       .WithReference(userApi);
+       .WithReference(userApi)
+       .WaitFor(weatherApi)
+       .WaitFor(userApi);
 
 builder.Build().Run();
